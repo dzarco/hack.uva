b2f5aaa27cd42e9bb7c99bc6ed79aac80c0c7758
sg2nq <Siddhant@d-172-25-165-18.dhcp.virginia.edu>
2017-03-25 20:15:33 -0400

    BACKEND LIT

--- NameSuggestions.py
+++ NameSuggestions.py
@@ -2,10 +2,27 @@ import urllib.request
 import bs4
 import json
 
+def facebook_search(a):
+    try:
+        data = urllib.request.urlopen("http://facebook.com/"+a)
+        bool = False
+    except urllib.error.HTTPError:
+        bool = True
+    return bool
+
+
+def twitter_search(a):
+    try:
+        data = urllib.request.urlopen("http://twitter.com/"+a)
+        bool = False
+    except urllib.error.HTTPError:
+        bool = True
+    return bool
+
+
 def ListOfElements(a):
     web = urllib.request.urlopen("http://naming.verisign-grs.com/ns-api/1.0/suggest?key="+a)
     page = web.read()
-
     parsedPage = bs4.BeautifulSoup(page, "html.parser")
     original_string = str(parsedPage)
     parsed_string = original_string.split("[{",1)[-1]
@@ -14,4 +31,16 @@ def ListOfElements(a):
     listofelemnts=[]
     for x in parsedlist:
         listofelemnts.append(x.replace("\"","").split(","))
+    for x in listofelemnts:
+        name=((x[0]).split(":", 1)[-1]).split(".", 1)[0]
+        if facebook_search(name):
+            x.append("Facebook:Available")
+        else:
+            x.append("Facebook:Unavailable")
+        if twitter_search(name):
+            x.append("Twitter:Available")
+        else:
+            x.append("Twitter:Unavailable")
     return listofelemnts
+
+print(ListOfElements("Ryan"))
